53134bcb043e90aa44560539ea98bcecf7cc0e27
c0ncon <c4rneade@gmail.com>
2020-07-09 10:23:47 +0900

    using powerlevel10k

--- oh-my-zsh/envs.zsh
+++ oh-my-zsh/envs.zsh
@@ -1,4 +1,4 @@
-ZSH_THEME="gnzh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 export PATH="$HOME/wsl-shims:$HOME/.cargo/bin:$HOME/bin:$HOME/.rbenv/bin:$PATH"
 
@@ -6,3 +6,7 @@ export PATH="$HOME/wsl-shims:$HOME/.cargo/bin:$HOME/bin:$HOME/.rbenv/bin:$PATH"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 umask 002
+
+# https://github.com/romkatv/powerlevel10k
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
